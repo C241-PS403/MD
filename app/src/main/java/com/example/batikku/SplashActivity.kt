090dc313534104f7c80b43031dd33d607360fce2
Wisnu12 <jasonmasonA45@gmail.com>
2024-05-29 12:44:34 +0700

    mindahin dr branch

---
+++ app/src/main/java/com/example/batikku/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.batikku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val Splash_time_out : Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, Splash_time_out)
+    }
+    }
