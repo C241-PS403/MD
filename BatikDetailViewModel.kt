ac8b7accec0fcf2a6942ffcdd917391c82fd049b
Raihandsyahendra <100402017+Raihandsyahendra@users.noreply.github.com>
2024-06-17 00:34:00 +0700

    list& detail act

---
+++ BatikDetailViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.batikku.view.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.batikku.view.Api.ApiConfig
+import com.example.batikku.view.Model.ResponseBatikDetail
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class BatikDetailViewModel : ViewModel() {
+
+    private val apiService = ApiConfig.getService()
+
+    private val _batikDetail = MutableLiveData<ResponseBatikDetail>()
+    val batikDetail: LiveData<ResponseBatikDetail>
+        get() = _batikDetail
+
+    fun getBatikDetail(batikId: Int) {
+        apiService.getBatikDetail(batikId).enqueue(object : Callback<ResponseBatikDetail> {
+            override fun onResponse(call: Call<ResponseBatikDetail>, response: Response<ResponseBatikDetail>) {
+                if (response.isSuccessful) {
+                    _batikDetail.value = response.body()
+                } else {
+                    // Handle error response
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBatikDetail>, t: Throwable) {
+                // Handle network failure
+            }
+        })
+    }
+}
